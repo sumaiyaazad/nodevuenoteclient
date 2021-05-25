4392675810f9c8c77ec16a9c89f9572541e6db2f
sumaiyaazad <azadsumaiya00@gmail.com>
2021-05-26 00:30:58 +0600

    docker 4

--- src/main.js
+++ src/main.js
@@ -17,7 +17,8 @@ import VueAxios from 'vue-axios'
 import {store} from "./store";
 
 // axios.defaults.baseURL='https://mt-notebook.herokuapp.com/';
-axios.defaults.baseURL='https://mt-notebook-backend:4000';
+// axios.defaults.baseURL='http://192.168.99.100:4000';
+axios.defaults.baseURL='http://mt-notebook-backend:4000';
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
 
